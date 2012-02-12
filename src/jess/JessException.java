beb4b49b55539140c8d46481a637d7d684a1bd66
Vinod Muthusamy <vinod.muthusamy@gmail.com>
2012-02-11 19:06:55 -0500

    Stub out Jess classes so the project builds (temporary solution).

---
+++ src/jess/JessException.java
@@ -0,0 +1,22 @@
+package jess;
+
+public class JessException extends java.lang.Exception implements java.io.Serializable{
+    public JessException(java.lang.String s1, java.lang.String s2, java.lang.String s3) {};
+//    public JessException(java.lang.String, java.lang.String, int) {};
+//    public JessException(java.lang.String, java.lang.String, java.lang.Throwable) {};
+//    public java.lang.Throwable getNextException() {};
+//    public void setNextException(java.lang.Throwable) {};
+//    public int getLineNumber() {};
+//    public void setLineNumber(int) {};
+//    public java.lang.String getMessage() {};
+//    public void setMessage(java.lang.String);
+//    public java.lang.String getRoutine();
+//    public void setRoutine(java.lang.String);
+//    public java.lang.String getData();
+//    public void setData(java.lang.String);
+//    public java.lang.String getProgramText();
+//    public void setProgramText(java.lang.String);
+//    public void addContext(java.lang.String);
+//    public java.lang.String getContext();
+//    public java.lang.String toString();
+}
