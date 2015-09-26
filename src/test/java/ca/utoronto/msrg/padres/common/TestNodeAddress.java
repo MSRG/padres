dbedadf58e921173dae9846946006ddacbd381b9
Christoph Doblander <chris@degree.at>
2015-09-26 10:52:46 +0200

    tests for nodeaddress parsing

---
+++ src/test/java/ca/utoronto/msrg/padres/common/TestNodeAddress.java
@@ -0,0 +1,47 @@
+package ca.utoronto.msrg.padres.common;
+
+import ca.utoronto.msrg.padres.common.comm.CommunicationException;
+import ca.utoronto.msrg.padres.common.comm.ConnectionHelper;
+import ca.utoronto.msrg.padres.common.comm.INodeAddress;
+import ca.utoronto.msrg.padres.common.comm.rmi.RMIAddress;
+import ca.utoronto.msrg.padres.common.comm.socket.SocketAddress;
+import ca.utoronto.msrg.padres.common.comm.zero.ZeroAddress;
+import junit.framework.TestCase;
+
+import static ca.utoronto.msrg.padres.common.comm.ConnectionHelper.getLocalIPAddr;
+
+/**
+ * Created by chris on 26.09.15.
+ */
+public class TestNodeAddress extends TestCase {
+
+    public void test_parsing_rmi_address() throws CommunicationException {
+        String uri = "rmi://127.0.0.1:8080/rmi1";
+
+        INodeAddress address = ConnectionHelper.getAddress(uri);
+
+        assertEquals("rmi1", address.getNodeID());
+        assertEquals(RMIAddress.class, address.getClass());
+        assertEquals(8080, address.getPort());
+    }
+
+    public void test_parsing_socket_address() throws CommunicationException {
+        String uri = "socket://127.0.0.1:8080/sock1";
+
+        INodeAddress address = ConnectionHelper.getAddress(uri);
+
+        assertEquals("sock1", address.getNodeID());
+        assertEquals(SocketAddress.class, address.getClass());
+        assertEquals(8080, address.getPort());
+    }
+
+    public void test_parsing_zero_address() throws CommunicationException {
+        String uri = "zero-tcp://127.0.0.1:8080/zerotcp";
+
+        INodeAddress address = ConnectionHelper.getAddress(uri);
+
+        assertEquals("zerotcp", address.getNodeID());
+        assertEquals(ZeroAddress.class, address.getClass());
+        assertEquals(8080, address.getPort());
+    }
+}
