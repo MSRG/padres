c3a6c65f0bafcb010170ddc0800aa6428ae43c66
Christoph Doblander <christoph.doblander@in.tum.de>
2015-11-12 14:02:59 +0100

    fixed testcyclicheartbeat

--- src/test/java/ca/utoronto/msrg/padres/integration/cyclic/TestCyclicHeartBeat.java
+++ src/test/java/ca/utoronto/msrg/padres/integration/cyclic/TestCyclicHeartBeat.java
@@ -1,6 +1,18 @@
 package ca.utoronto.msrg.padres.integration.cyclic;
 
+import ca.utoronto.msrg.padres.AllTests;
+import ca.utoronto.msrg.padres.MessageWatchAppender;
+import ca.utoronto.msrg.padres.PatternFilter;
+import ca.utoronto.msrg.padres.broker.brokercore.BrokerConfig;
+import ca.utoronto.msrg.padres.broker.brokercore.BrokerCore;
+import ca.utoronto.msrg.padres.broker.brokercore.BrokerCoreException;
+import ca.utoronto.msrg.padres.broker.brokercore.InputQueueHandler;
+import ca.utoronto.msrg.padres.common.util.LogSetup;
+import ca.utoronto.msrg.padres.integration.tester.GenericBrokerTester;
+import ca.utoronto.msrg.padres.integration.tester.TesterBrokerCore;
+import ca.utoronto.msrg.padres.tools.padresmonitor.MonitorFrame;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,19 +28,64 @@ import static ca.utoronto.msrg.padres.AllTests.setupConfigurations;
  *
  * @author Bala Maniymaran
  */
-public class TestCyclicHeartBeat extends TestHeartBeat {
+public class TestCyclicHeartBeat extends Assert {
+
+    protected GenericBrokerTester _brokerTester;
+
+    protected BrokerCore brokerCore1;
+
+    protected BrokerCore brokerCore2;
+
+    protected MonitorFrame padresMonitor;
+
+    protected MessageWatchAppender messageWatcher;
+
+    protected PatternFilter msgFilter;
 
     @Before
     public void setUp() throws Exception {
         setupConfigurations(5, "socket");
-        super.setUp();
+
+        _brokerTester = new GenericBrokerTester();
+
+        // start the broker
+        AllTests.setupStarNetwork01();
+        brokerCore1 = createNewBrokerCore(AllTests.brokerConfig01);
+        brokerCore1.initialize();
+        brokerCore2 = createNewBrokerCore(AllTests.brokerConfig02);
+        brokerCore2.initialize();
+        // start monitor for broker1
+        padresMonitor = new MonitorFrame();
+        padresMonitor.getOverlayManager().connect(brokerCore1.getBrokerURI());
+        padresMonitor.getOverlayManager().setHeartbeatParameters(brokerCore1.getBrokerID(), true,
+                3000, 5000, 2);
+        // setup filters and message watcher
+        messageWatcher = new MessageWatchAppender();
+        msgFilter = new PatternFilter(InputQueueHandler.class.getName());
+        msgFilter.setPattern("");
+        messageWatcher.addFilter(msgFilter);
+        LogSetup.addAppender("MessagePath", messageWatcher);
+    }
+
+    protected BrokerCore createNewBrokerCore(BrokerConfig brokerConfig) throws BrokerCoreException {
+        return new TesterBrokerCore(_brokerTester, brokerConfig);
     }
 
     @After
     public void tearDown() throws Exception {
-        super.tearDown();
-    }
 
+        padresMonitor.shutdown();
+        brokerCore1.shutdown();
+        brokerCore2.shutdown();
+        LogSetup.removeAppender("MessagePath", messageWatcher);
+        padresMonitor = null;
+
+        // TODO: remove this line
+        // padresMonitor.exitMonitor();
+        brokerCore1 = null;
+        brokerCore2 = null;
+        _brokerTester = null;
+    }
 
     /**
      * Test the HeartBeat ACK publication is sent back to the source broker, where broker1.HeartBeat
@@ -80,7 +137,7 @@ public class TestCyclicHeartBeat extends TestHeartBeat {
                                 addPredicate("detectorID", "eq", brokerCore1.getBrokerID()),
                         brokerCore1.getBrokerURI() + "-" + padresMonitor.getMonitorID());
         padresMonitor.getOverlayManager().stopBroker(brokerCore2.getBrokerID());
-        assertTrue(_brokerTester.waitUntilExpectedEventsHappen());
+        assertTrue(_brokerTester.waitUntilExpectedEventsHappen(16000));
 
         // Resume broker 2 and wait for the failure to be cleared.
         // TODO: Also test for tid attribute?
