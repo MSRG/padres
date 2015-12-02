862eab81427d69f007762052e2e97eac07573046
Christoph Doblander <christoph.doblander@in.tum.de>
2015-12-02 09:46:10 +0100

    decreased timeouts

--- src/test/java/ca/utoronto/msrg/padres/integration/tester/GenericBrokerTester.java
+++ src/test/java/ca/utoronto/msrg/padres/integration/tester/GenericBrokerTester.java
@@ -33,9 +33,9 @@ import ca.utoronto.msrg.padres.common.message.UnsubscriptionMessage;
  */
 public class GenericBrokerTester implements IBrokerTester {
 
-    public static int DEFAULT_WAIT = 20000;
-	public static int DEFAULT_SHUTDOWN_WAIT = 35000; // millisec.
-	public static int DEFAULT_START_WAIT = 22000; // millisec.
+    public static int DEFAULT_WAIT = 10000;
+	public static int DEFAULT_SHUTDOWN_WAIT = 15000; // millisec.
+	public static int DEFAULT_START_WAIT = 12000; // millisec.
 	public static boolean PRODUCE_PRINT_TRACES = false;
 	public static boolean lookAtPubs = false;
 	public static boolean lookAtSubs = false;
